fef3d8b80d27fa5d5176cedbdd01574d895e46fb
daisy <rtw165@gmail.com>
2026-05-31 14:27:59 +0800

    優化程式

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+&k&ts*^11q@=pxj2271#nyy*lz&sx^l!398@0tsx0$$*3i^qj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
